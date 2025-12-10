ebf75913ed2de5b1f6559b2a14aa7f41e438a11c
Omni1210 <jadd.almonte@gmail.com>
2025-12-10 21:11:19 +0800

    Update db.mjs

--- db.mjs
+++ db.mjs
@@ -1,11 +1,9 @@
 import mysql from "mysql2/promise";
 
 export const db = await mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "root",
-  database: "gateway_launches",
+  host: "sql12.freesqldatabase.com",
+  user: "sql12811398",
+  password: "YOUR_PASSWORD_HERE", // wait for it to finish loading
+  database: "sql12811398",
   port: 3306
 });
-
-
